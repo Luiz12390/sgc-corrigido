bf31fe68bd658b61a4a2d8cfa2f528bbe2667514
Luiz12390 <moreiraluiz@unochapeco.edu.br>
2025-09-02 03:13:39 -0300

    Ajustado paginação

--- resources/views/livewire/challenges-index.blade.php
+++ resources/views/livewire/challenges-index.blade.php
@@ -89,8 +89,48 @@
                 </div>
             </section>
 
-            <div>
-                {{ $challenges->links() }}
+            <div class="pagination-container">
+                @if ($challenges->hasPages())
+                    <nav>
+                        <ul class="pagination">
+                            @if ($challenges->onFirstPage())
+                                <li class="page-item disabled" aria-disabled="true">
+                                    <span class="page-link">&lt;</span>
+                                </li>
+                            @else
+                                <li class="page-item">
+                                    <a class="page-link" href="{{ $challenges->previousPageUrl() }}" rel="prev">&lt;</a>
+                                </li>
+                            @endif
+
+                            @foreach ($challenges->links()->elements as $element)
+                                @if (is_string($element))
+                                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                                @endif
+
+                                @if (is_array($element))
+                                    @foreach ($element as $page => $url)
+                                        @if ($page == $challenges->currentPage())
+                                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                                        @else
+                                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                                        @endif
+                                    @endforeach
+                                @endif
+                            @endforeach
+
+                            @if ($challenges->hasMorePages())
+                                <li class="page-item">
+                                    <a class="page-link" href="{{ $challenges->nextPageUrl() }}" rel="next">&gt;</a>
+                                </li>
+                            @else
+                                <li class="page-item disabled" aria-disabled="true">
+                                    <span class="page-link">&gt;</span>
+                                </li>
+                            @endif
+                        </ul>
+                    </nav>
+                @endif
             </div>
         </main>
     </div>
